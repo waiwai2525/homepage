f89fbcfad5f63e0054b044935d2460a91f058d4d
waiwai2525 <waiwai.cps@gmail.com>
2022-07-01 21:40:47 +0900

    style: Run prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
-}
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
